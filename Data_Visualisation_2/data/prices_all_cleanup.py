df590c8a463f49c03a89a4122270150ca05d1146
ShenyiHu <shuu0020@student.monash.edu>
2022-10-14 12:41:41 +1100

    Added cheapest and most expensive in prices_all.csv.

--- Data_Visualisation_2/data/prices_all_cleanup.py
+++ Data_Visualisation_2/data/prices_all_cleanup.py
@@ -6,9 +6,9 @@ output_file = open('prices_all.csv', 'w')
 list = []
 
 for col in input_file:
-    list.append((col['Name'], col['Average price of 1GB (USD - at the start of 2020)'], col['Average price of 1GB (USD - at the start of 2021)'], col['Average price of 1GB (USD)']))
+    list.append((col['Name'], col['Average price of 1GB (USD - at the start of 2020)'], col['Average price of 1GB (USD - at the start of 2021)'], col['Average price of 1GB (USD)'], col['Cheapest 1GB for 30 days (USD)'], col['Most expensive 1GB (USD)']))
 
-csv.writer(output_file).writerow(['Country', 'Price (2020)', 'Price (2021)', 'Price (2022)'])
+csv.writer(output_file).writerow(['Country', 'Price (2020)', 'Price (2021)', 'Price (2022)', 'Cheap', 'Expensive'])
 
 for data in list:
     country = data[0]
@@ -16,9 +16,11 @@ for data in list:
         price_2020 = float(data[1][1:])
         price_2021 = float(data[2][1:])
         price_2022 = float(data[3][1:])
+        price_cheap = float(data[4][1:])
+        price_expensive = float(data[5][1:])
     except:
         continue
 
-    csv.writer(output_file).writerow([country, price_2020, price_2021, price_2022])
+    csv.writer(output_file).writerow([country, price_2020, price_2021, price_2022, price_cheap, price_expensive])
 
 output_file.close()
