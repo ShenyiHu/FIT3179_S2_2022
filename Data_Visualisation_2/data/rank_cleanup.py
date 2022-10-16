71dc7d77af4167c7fbc375ec0a2816739738d756
ShenyiHu <shuu0020@student.monash.edu>
2022-10-17 01:20:07 +1100

    Fixed data error.

--- Data_Visualisation_2/data/rank_cleanup.py
+++ Data_Visualisation_2/data/rank_cleanup.py
@@ -1,35 +1,32 @@
 import csv
 
 input_file = csv.DictReader(open('users_speed_prices.csv', 'r', encoding = 'ISO-8859-1'))
-output_file = open('users_speed_prices.csv', 'w')
+output_file = open('rank.csv', 'w')
 
 tup_list = []
 for col in input_file:
-    tup_list.append((col['Continent'], col['Country'], col['Population'], col['Users'], col['Price'], col['Year']))
+    tup_list.append((col['Continent'], col['Country'], col['Population'], col['Users'], col['Speed'], col['Price'], col['Year']))
 
 tup_list1 = []
 for tup in tup_list:
-    if tup[5] == "2022":
+    if tup[6] == "2022":
         tup_list1.append(tup)
 
-tup_list1.sort(key=lambda tup: tup[4], reverse=True)
-print(tup_list1)
+tup_list1.sort(key=lambda tup: tup[5], reverse=True)
 i = 1
 tup_list2 = []
 for tup in tup_list1:
     tup_list2.append(tup + (i,))
     i += 1
 
-# print(tup_list2)
-
 csv.writer(output_file).writerow(['Continent', 'Country', 'Population', 'Users', 'Speed', "Price", "Year", "Rank"])
 for tup in tup_list:
     for tup1 in tup_list2:
         if tup[1] == tup1[1]:
-            if tup[5] == "2022":
-                tup += (tup1[6],)
+            if tup[6] == "2022":
+                tup += (tup1[7],)
             else:
                 tup += ("N/A",)
-            csv.writer(output_file).writerow([tup[0], tup[1], tup[2], tup[3], tup[4], tup[5], tup[6]])
+            csv.writer(output_file).writerow([tup[0], tup[1], tup[2], tup[3], tup[4], tup[5], tup[6], tup[7]])
 
 output_file.close()
